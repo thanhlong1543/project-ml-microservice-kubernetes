34ad2c5ce76eb6c74ce6b7448231f79dd57d4806
EC2 Default User <ec2-user@ip-172-31-20-168.ec2.internal>
2022-10-30 15:22:22 +0000

    implement to task5

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,12 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=thanhlong1543/ml-microservice:v1.0
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login && docker tag ml-microservice $dockerpath
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
